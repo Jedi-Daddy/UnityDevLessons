96e412c95f521a2bfa772d677ef2443c3b7be04d
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-07-29 12:43:44 +0200

    Add unity lessons

---
+++ Unity Essentials/Unity Essentials/Assets/Scripts/TestScript.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public string myMessage;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(myMessage);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(myMessage);
+    }
+}
