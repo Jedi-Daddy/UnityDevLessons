c647af63f802849ddb317ca22ad8fc806669196c
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-26 23:06:09 +0200

    add obstacle and add PlayerController script

---
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // We'll move the cehicle forward
+        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    }
+}
