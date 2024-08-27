ceaa44f7920004745b46c786dfed49b55f794840
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-27 12:52:37 +0200

    change color for playmode (R=157,G=194,B=239,A=150)

--- Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
+++ Create with Code/Prototype 1/Assets/Scripts/FollowPlayer.cs
@@ -14,7 +14,7 @@ public class FollowPlayer : MonoBehaviour
     }
 
     // Update is called once per frame
-    void Update()
+    void LateUpdate()
     {
         // Offset the camera behind the player
         transform.position = player.transform.position + offset;
