72a77cc34731e16688cfa1469d2d79ac7c5d2e14
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-28 13:13:49 +0200

    Plane programming challenge

---
+++ Create with Code/Prototype 1/Assets/Challenge 1/Scripts/FollowPlayerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlayerX : MonoBehaviour
+{
+    public GameObject player;
+    private Vector3 offset = new Vector3(20, 0, 0);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        // Offset the camera behind the player
+        transform.position = player.transform.position + offset;
+    }
+}
---
+++ Create with Code/Prototype 1/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 10f;
+    public float rotationSpeed = 100f;
+    public float verticalInput;
+    public float horizontalInput;
+    public float propSpeed = 50f;
+
+    //get position|location of the propeller
+    public Transform propeller;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        // get the user's vertical input
+        verticalInput = Input.GetAxis("Vertical");
+        horizontalInput = Input.GetAxis("Horizontal");
+
+        // move the plane forward at a constant rate
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        // tilt the plane up/down based on up/down arrow keys
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime * verticalInput);
+        transform.Rotate(Vector3.back * rotationSpeed * Time.deltaTime * horizontalInput);
+
+        //rotate the propeller
+        propeller.Rotate(Vector3.forward * propSpeed * Time.deltaTime);
+    }
+}
