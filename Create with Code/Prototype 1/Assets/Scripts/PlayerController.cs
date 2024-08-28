2954bafcc1fa765c8398f91eac95c7e41d10da4b
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-28 10:04:44 +0200

    add verticalInput

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -7,6 +7,7 @@ public class PlayerController : MonoBehaviour
     public float speed = 5.0f;
     public float turnSpeed;
     public float horizontalInput;
+    public float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,9 +20,10 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
 
         // We'll move the cehicle forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
         transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
