c7e9814f00e64c43a1a9dded4a1bd8c73c7aead8
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-28 10:17:54 +0200

    add empty for the obstacles and change public variables to private

--- Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
+++ Create with Code/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    public float speed = 5.0f;
-    public float turnSpeed;
-    public float horizontalInput;
-    public float forwardInput;
+    // Private Variables
+    private float speed = 5.0f;
+    private float turnSpeed = 25.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     // Start is called before the first frame update
     void Start()
@@ -19,11 +20,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // This is where we get player input
         horizontalInput = Input.GetAxis("Horizontal");
         forwardInput = Input.GetAxis("Vertical");
 
-        // We'll move the cehicle forward
+        // We'll move the vehicle forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // We turn vehicle 
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
