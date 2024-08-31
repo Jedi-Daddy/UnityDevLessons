310a7c8318e3e704cf0efa305aea6a81d32031a1
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-08-31 21:04:23 +0200

    add Player speed

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
+    public float speed = 10.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,5 +17,6 @@ public class PlayerController : MonoBehaviour
     void Update()
     {
         horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
 }
