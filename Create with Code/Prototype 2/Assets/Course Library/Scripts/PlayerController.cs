675cbb91c72b77dbc517726765b769d4bdf0e7ea
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-01 20:57:39 +0200

    edit player controller

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -16,6 +16,10 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
     }
