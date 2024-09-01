2d9c41c7acd26d5fffdceb4a1b10721df101d544
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-01 21:00:07 +0200

    add xRange to PlayerController for movement

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15;
 
     // Start is called before the first frame update
     void Start()
@@ -16,9 +17,13 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (transform.position.x < -15)
+        if (transform.position.x < -xRange)
         {
-            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
