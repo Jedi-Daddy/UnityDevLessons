6cf2834f926be179787927fd520e118a85a1fedd
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-02 08:53:55 +0200

    add animals prefab

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -30,6 +30,7 @@ public class PlayerController : MonoBehaviour
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
 
+        
         if (Input.GetKeyDown(KeyCode.Space))
         {
             // Launch a projectile from the player
