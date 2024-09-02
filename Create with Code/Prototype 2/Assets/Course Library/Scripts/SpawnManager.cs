fcf63942be770f485eacf5fbd4ebe9a82f01bd82
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-02 12:46:01 +0200

    add random spawn for animals

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/SpawnManager.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefab;
+    private float spawnRangeX = 20;
+    private float spawnPosZ = 20;
 
     // Start is called before the first frame update
     void Start()
@@ -15,6 +17,13 @@ public class SpawnManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            // Randomly generate animal index and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefab.Length);
+
+            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
+        }
     }
 }
