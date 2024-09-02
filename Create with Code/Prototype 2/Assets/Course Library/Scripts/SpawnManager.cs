ae6dcc7b2e110fb757abf1951e18d9a5e2e52583
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-02 13:21:29 +0200

    create method for random spawn with intervals

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/SpawnManager.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/SpawnManager.cs
@@ -5,25 +5,29 @@ using UnityEngine;
 public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefab;
-    private float spawnRangeX = 20;
-    private float spawnPosZ = 20;
+    private float spawnRangeX = 15;
+    private float spawnPosZ = 15;
+    private float startDelay = 2;
+    private float spawnInterval = 1.5f;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.S))
-        {
-            // Randomly generate animal index and spawn position
-            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
-            int animalIndex = Random.Range(0, animalPrefab.Length);
+       
+    }
 
-            Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
-        }
+    void SpawnRandomAnimal()
+    {
+        // Randomly generate animal index and spawn position
+        Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+        int animalIndex = Random.Range(0, animalPrefab.Length);
+
+        Instantiate(animalPrefab[animalIndex], spawnPos, animalPrefab[animalIndex].transform.rotation);
     }
 }
