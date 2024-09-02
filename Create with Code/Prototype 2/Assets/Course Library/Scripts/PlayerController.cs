38dfa74ee63517d7e20559a7b90ad90d8e50b44f
Jedi-Daddy <av.zhuravlev90@gmail.com>
2024-09-02 08:41:52 +0200

    create new pizza prefab and add projectilePrefab to Player

--- Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
+++ Create with Code/Prototype 2/Assets/Course Library/Scripts/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 15;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
